a08de686e3e6ecd73250f8e9144c076748a8da1b
prachitid02 <79130948+prachitid02@users.noreply.github.com>
2022-04-02 15:26:19 +0530

    Add files via upload

---
+++ To do list/script.js
@@ -0,0 +1,26 @@
+let form = document.querySelector('form.row');
+let ul = document.querySelector('.task_list');
+
+let htmlTemplate = function(todo){
+    let li = `
+        <li>${todo}<span class="del">X</span> </li>
+    `;
+    ul.innerHTML += li;
+}
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    let todo = form.task.value.trim();
+    if(todo.length){
+        htmlTemplate(todo);
+        form.reset();
+    }
+    
+})
+
+ul.addEventListener('click', e => {
+    if(e.target.classList.contains('del')){
+        e.target.parentElement.remove();
+    }
+    
+})
